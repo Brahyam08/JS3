002e8ee4008b3775b60ec83320e4661bb8823897
brahyam08 <brahyam.agudelo@gmail.com>
2021-04-22 17:07:55 +0200

    fix1

--- ej11.js
+++ ej11.js
@@ -1,6 +1,6 @@
 let num1 = parseInt(window.prompt('dime un numero'));
 let num2 = parseInt(window.prompt('dime otro numero'));
-
+//falta limitador de numeros
 if (parseInt((num1 % 100)/10) == parseInt((num2 % 100)/10)){
     console.log('el primer digito es comun entre ellos')
 }else if((parseInt(num1 % 10)  == parseInt(num2 % 10))) {
